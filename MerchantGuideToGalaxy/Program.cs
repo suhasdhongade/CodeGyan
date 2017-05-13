6c5e3b3c9f0f593406d78922cdb31bb932d0f584
suhasdhongade <suhasdhongade@gmail.com>
2017-05-13 17:05:11 +0530

    Data

--- MerchantGuideToGalaxy/Program.cs
+++ MerchantGuideToGalaxy/Program.cs
@@ -10,6 +10,28 @@ namespace MerchantGuideToGalaxy
     {
         static void Main(string[] args)
         {
+            List<string> UserInput = new List<string>();
+
+            UserInput.Add("glob is I");
+            UserInput.Add("prok is V");
+            UserInput.Add("pish is X");
+            UserInput.Add("tegj is L");
+            UserInput.Add("jac is C");
+            UserInput.Add("spa is M");
+
+            UserInput.Add("glob glob glob Silver is 34 Credits");
+            UserInput.Add("glob prok Gold is 57800 Credits");
+            UserInput.Add("pish pish Iron is 3910 Credits");
+
+            //Questions
+            UserInput.Add("how much is spa jac spa pish tegj glob prok ?");
+            UserInput.Add("how much is pish tegj glob glob?");
+            UserInput.Add("how many Credits is glob prok Silver ?");
+            //UserInput.Add("how many Credits is glob prok Gold ?");
+            //UserInput.Add("how many Credits is glob prok Iron ?");
+            //UserInput.Add("how much wood could a woodchuck chuck is if a woodchuck could chuck wood ?");
+
+            Problem.Process(UserInput);
 
         }
     }
